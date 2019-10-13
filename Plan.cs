ed579d3df00355b60490d410263063d083d327c0
Bingo <artur.tunevic@gmail.com>
2019-10-13 21:52:25 +0300

    Add

--- Plan.cs
+++ Plan.cs
@@ -9,5 +9,23 @@ namespace MobPlanaiEuklidas
     public class Plan
     {
 
+        private string provider;
+        private string planName;
+        private int minAmt;
+        private int smsAmt;
+        private int netGBAmt;
+        private double price;
+
+        public Plan(string provider, string planName, int minAmt, int smsAmt, int netGBAmt, double price)
+        {
+            this.provider = provider;
+            this.planName = planName;
+            this.minAmt = minAmt;
+            this.smsAmt = smsAmt;
+            this.netGBAmt = netGBAmt;
+            this.price = price;
+        }
+
+
     }
 }
